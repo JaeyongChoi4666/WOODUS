676cdd53f365eebc11b059ca4761017df9fca1a3
JaeyongChoi4666 <81898000+JaeyongChoi4666@users.noreply.github.com>
2023-04-05 21:53:55 +0900

    Initial commit

---
+++ src/main/java/com/example/woodus/WoodusApplication.java
@@ -0,0 +1,13 @@
+package com.example.woodus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoodusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WoodusApplication.class, args);
+	}
+
+}
